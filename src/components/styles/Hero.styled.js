8e880933b3f875943c8f5a5c2bb4953cdc7078dd
codernami <106401441+codernami@users.noreply.github.com>
2022-05-29 19:02:26 -0300

    Add responsive hero

--- src/components/styles/Hero.styled.js
+++ src/components/styles/Hero.styled.js
@@ -8,15 +8,39 @@ export const StyledHero = styled.div`
   background-color: #ebfbff;
   text-align: center;
 
+  @media (min-width: 768px) {
+    flex-direction: row;
+    padding: 3rem;
+    text-align: left;
+  }
+
+  div {
+    @media (min-width: 768px) {
+      width: 50%;
+    }
+  }
+
   h2 {
     margin: 1rem 0;
   }
 
+  p {
+    @media (min-width: 768px) {
+      width: 95%;
+      line-height: 1.5;
+    }
+  }
+
   button {
     margin: 2rem;
+
+    @media (min-width: 768px) {
+      margin-left: 0;
+      padding: 1rem 2rem;
+    }
   }
 
   img {
-    width: 80%;
+    width: 100%;
   }
 `;
